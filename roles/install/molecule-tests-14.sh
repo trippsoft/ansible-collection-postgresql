d3b676113980dd3b73d81cf463a23ea9ce610555
Jim Tarpley <46848743+trippsc2@users.noreply.github.com>
2024-06-29 20:03:00 -0400

    Improved molecule tests.

--- roles/install/molecule-tests-14.sh
+++ roles/install/molecule-tests-14.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-MOLECULE_BOX="rocky8_cis" molecule test -s 14
-MOLECULE_BOX="debian12_base" molecule test -s 14
-MOLECULE_BOX="ubuntu2404_base" molecule test -s 14
+MOLECULE_BOX="rocky8_cis" MOLECULE_PGSQL_MAJOR_VERSION=14 molecule test
+MOLECULE_BOX="debian12_base" MOLECULE_PGSQL_MAJOR_VERSION=14 molecule test
+MOLECULE_BOX="ubuntu2404_base" MOLECULE_PGSQL_MAJOR_VERSION=14 molecule test
 
-MOLECULE_BOX="debian11_base" molecule test -s 14
+MOLECULE_BOX="debian11_base" MOLECULE_PGSQL_MAJOR_VERSION=14 molecule test
 
-MOLECULE_BOX="rocky9_base" molecule test -s 14
+MOLECULE_BOX="rocky9_base" MOLECULE_PGSQL_MAJOR_VERSION=14 molecule test
 
-MOLECULE_BOX="ubuntu2204_base" molecule test -s 14
-MOLECULE_BOX="ubuntu2004_base" molecule test -s 14
+MOLECULE_BOX="ubuntu2204_base" MOLECULE_PGSQL_MAJOR_VERSION=14 molecule test
+MOLECULE_BOX="ubuntu2004_base" MOLECULE_PGSQL_MAJOR_VERSION=14 molecule test
